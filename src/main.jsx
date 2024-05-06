b506b1a07ed1526578ec3879807b58e97a49093f
Felipe-Focil <felipe.focil@omegaup.com>
2024-05-05 21:14:47 -0600

    routering

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Home from "./components/Home.jsx";
+import Contact from "./components/Contact.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/OFMIScoreboard/",
+    element: <App />,
+    children: [
+      {
+        path: "/OFMIScoreboard/",
+        element: <Home />,
+      },
+      {
+        path: "/OFMIScoreboard/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
