b91cfa2cc36abf59fb76251220fa15779b4677a2
Felipe-Focil <felipe.focil@omegaup.com>
2024-05-06 01:26:51 -0600

    Add content

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Link, Outlet } from "react-router-dom";
+import Toolbar from "@mui/material/Toolbar";
+import ThemeToggleButton from "./components/ThemeToggleButton";
+import FeaturedContests from "./components/FeaturedContests";
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-
-      <nav>
-        <Link to="/OFMIScoreboard/">Home</Link>
-        {" | "}
-        <Link to="/OFMIScoreboard/contact">Contact</Link>
-      </nav>
-
-      <Outlet />
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>OFMI</h1>
+      <FeaturedContests />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,82 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import Home from "./components/Home.jsx";
-import Contact from "./components/Contact.jsx";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Toolbar from "@mui/material/Toolbar";
+import Contest from "./components/Contest.jsx";
+import SplitContest from "./components/SplitContest.jsx";
+import StackComponent from "./components/StackComponent.jsx";
+import FullScreenButton from "./components/FullScreenButton.jsx";
+import ThemeToggleButton from "./components/ThemeToggleButton";
+import { lightTheme, darkTheme } from "./utils/Themes.jsx";
+import FeaturedContests from "./components/FeaturedContests.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import basename from "./utils/basename.jsx";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
-    path: "/OFMIScoreboard/",
+    path: `${basename}/`,
     element: <App />,
-    children: [
-      {
-        path: "/OFMIScoreboard/",
-        element: <Home />,
-      },
-      {
-        path: "/OFMIScoreboard/contact",
-        element: <Contact />,
-      },
-    ],
+  },
+  {
+    path: `${basename}/scoreboards`,
+    element: <SplitContest />,
+  },
+  {
+    path: `${basename}/contest`,
+    element: <Contest />,
+  },
+  {
+    path: `${basename}/contest/:id`,
+    element: <Contest />,
+  },
+  {
+    path: `${basename}/contest/admin/:id`,
+    element: <Contest admin={true} />,
+  },
+  {
+    path: `${basename}/merge`,
+    element: <StackComponent />,
   },
 ]);
 
+function WrappedApp() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const currentTheme = darkMode ? darkTheme : lightTheme;
+
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={currentTheme}>
+          <Toolbar>
+            <div style={{ flexGrow: 1 }} />
+            <ThemeToggleButton
+              theme={currentTheme}
+              toggleTheme={toggleDarkMode}
+              isDarkMode={darkMode}
+            />
+
+            <FullScreenButton />
+          </Toolbar>
+
+          <RouterProvider router={router}></RouterProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </>
+  );
+}
+
+// Renderiza tu aplicación
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <WrappedApp />
   </React.StrictMode>
 );
